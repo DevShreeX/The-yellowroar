08980a605cf24b60fe5732a187ac5d875848eafc
DevShreeX <gopikashreerathinakumar@gmail.com>
2026-05-13 12:54:53 +0530

    Add support staff page with split HTML/CSS/JS and new icons

---
+++ js/support.js
@@ -0,0 +1,6 @@
+// Support staff specific JavaScript
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Support staff page loaded");
+    
+    // Future interactive logic for support staff page can go here
+});
